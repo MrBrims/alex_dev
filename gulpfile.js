a28eafa1754004d17aafd3089635589cd381855a
MrBrims <magistrbrims@gmail.com>
2022-03-22 18:20:41 +0800

    added hero section

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,7 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
+    'node_modules/parallax-js/dist/parallax.min.js',
     'app/js/main.js'
   ])
     .pipe(concat('main.min.js'))
